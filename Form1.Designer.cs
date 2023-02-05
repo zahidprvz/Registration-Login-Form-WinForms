757cb53ad5c9efd6d69dd7c27312ea027d0e58f1
Zahid Pervaiz <20021519-143@uog.edu.pk>
2023-02-05 16:40:44 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace MyApplicationDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connect_db = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCloseForm = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.photoPbx = new System.Windows.Forms.PictureBox();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.OpenPhoto = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.photoPbx)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // connect_db
+            // 
+            this.connect_db.Location = new System.Drawing.Point(19, 114);
+            this.connect_db.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.connect_db.Name = "connect_db";
+            this.connect_db.Size = new System.Drawing.Size(267, 77);
+            this.connect_db.TabIndex = 0;
+            this.connect_db.Text = "Connect to DB";
+            this.connect_db.UseVisualStyleBackColor = true;
+            this.connect_db.Click += new System.EventHandler(this.connect_db_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 28);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(612, 75);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnCloseForm
+            // 
+            this.btnCloseForm.Location = new System.Drawing.Point(888, 28);
+            this.btnCloseForm.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCloseForm.Name = "btnCloseForm";
+            this.btnCloseForm.Size = new System.Drawing.Size(296, 77);
+            this.btnCloseForm.TabIndex = 2;
+            this.btnCloseForm.Text = "Close the Form";
+            this.btnCloseForm.UseVisualStyleBackColor = true;
+            this.btnCloseForm.Click += new System.EventHandler(this.btnCloseForm_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(351, 410);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Brand";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(504, 407);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(243, 44);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(504, 481);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(243, 44);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(351, 484);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 37);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Model";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(504, 561);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(243, 44);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(351, 564);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 37);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Year";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MyApplicationDB.Properties.Resources.car_rental;
+            this.pictureBox1.Location = new System.Drawing.Point(358, 176);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(389, 165);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // photoPbx
+            // 
+            this.photoPbx.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.photoPbx.Location = new System.Drawing.Point(798, 407);
+            this.photoPbx.Name = "photoPbx";
+            this.photoPbx.Size = new System.Drawing.Size(293, 198);
+            this.photoPbx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.photoPbx.TabIndex = 10;
+            this.photoPbx.TabStop = false;
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Location = new System.Drawing.Point(358, 654);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(160, 51);
+            this.browseBtn.TabIndex = 11;
+            this.browseBtn.Text = "Browse";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(543, 654);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(160, 51);
+            this.cancelBtn.TabIndex = 12;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(734, 654);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(160, 51);
+            this.saveBtn.TabIndex = 13;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // OpenPhoto
+            // 
+            this.OpenPhoto.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1227, 822);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.browseBtn);
+            this.Controls.Add(this.photoPbx);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCloseForm);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.connect_db);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.photoPbx)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connect_db;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCloseForm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox photoPbx;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.OpenFileDialog OpenPhoto;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace MyApplicationDB
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection myConnectionString = new SqlConnection("Data Source=ZAHIDPARVIZ;Initial Catalog=Car-CRUD-Zahid;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "Press the Button below if you want to connect to Database...";
+        }
+
+        private void connect_db_Click(object sender, EventArgs e)
+        {
+            myConnectionString.Open();
+
+            MessageBox.Show("We are connected to DB");
+            MessageBox.Show("We are disconnected to DB");
+
+
+            myConnectionString.Close();
+        }
+
+        private void btnCloseForm_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            clearData();
+        }
+
+        private void clearData()
+        {
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            photoPbx.Image = null;
+
+            textBox2.Focus();
+        }
+
+        private void browseBtn_Click(object sender, EventArgs e)
+        {
+            //It is a filter to open files in OpenDailogueBox
+            OpenPhoto.Filter = "jpeg|*.jpg|bmp|*.bmp|all files|*.*";
+
+            //To open a dialogueBox to select for photos
+            DialogResult res = OpenPhoto.ShowDialog();
+
+            //selection of Image
+            if (res == DialogResult.OK)
+            {
+                photoPbx.Image = Image.FromFile(OpenPhoto.FileName);
+            }
+            
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            if(textBox2.Text.Length < 1 || textBox3.Text.Length < 1 || textBox4.Text.Length < 1)
+            {
+                MessageBox.Show("Fill in all the fields");
+            }
+
+            else
+            {
+                try
+                {
+                    myConnectionString.Open();
+
+                    SqlCommand com = new SqlCommand("insert into Cars(Brand, Model, Year, Photo) values ('"+textBox2.Text+"', '"+textBox3.Text+"', "+Convert.ToInt32(textBox4.Text)+" , '"+photoPbx.Image+"')", myConnectionString);
+
+                    try
+                    {
+                        com.ExecuteNonQuery();
+
+                        MessageBox.Show("New Car saved");
+
+                        clearData();
+                    }
+
+                    catch (Exception exx)
+                    {
+
+                        MessageBox.Show("Failed to save data..!\n\nError Details: " + exx);
+                    }
+
+                    myConnectionString.Close();
+                }
+
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show("Failed to connect to DB\n\nError Details: " + ex);
+                }
+            }
+        }
+    }
+}
